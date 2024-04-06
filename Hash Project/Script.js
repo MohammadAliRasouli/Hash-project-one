92302fea98e51a33a7a93fd3dd4274e10e5b2c40
MohammadAliRasouli <135179327+MohammadAliRasouli@users.noreply.github.com>
2024-04-06 22:06:40 +0330

    Add files via upload

---
+++ Hash Project/Script.js
@@ -0,0 +1,23 @@
+console.log("Hi this is mmd");
+window.addEventListener("hashchange" , (e)=>{ router() })
+function router(){switch(location.hash){
+     case "" : render( {Title: "HOME" , color:"red" , href:"#home" })
+     break;
+     case"#home" : render({Title:"HOME" , color:"red" , href:"#home"})
+     break;
+     case "#search" : render({Title:"SEARCH" , color:"green" , href:"#search"})
+     break;
+     case "#likes" : render({Title:"LIKES" , color:"yellow" , href: "#likes"})
+     break;
+     case "#profile" : render({Title:"PROFILE" , color:"pink" , href: "#profile"})
+
+}}
+
+function render(data){document.querySelector("h1").innerText = data.Title ;
+ document.querySelector("main").style.backgroundColor = data.color ;
+ document.querySelector("title").style.innerText = data.Title  ;
+document.querySelectorAll("a").forEach((e)=>{if(e.href.includes(data.href))
+     {e.style.color = "black"  
+}else{e.style.color = "blue"}    })
+}
+
